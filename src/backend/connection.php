7151567aa5731ed88277f4217afb194eb0c2ade5
Aldo-Filho <aldinhuuu145@gmail.com>
2025-09-22 16:39:07 -0300

    Enviando Arquivos

--- src/backend/connection.php
+++ src/backend/connection.php
@@ -1,5 +1,5 @@
 <?php
-$connection = mysqli_connect("localhost", "root", "1234", "teste");
+$connection = mysqli_connect("kairoo.shop", "u136736371_user_kairoo", "Kairoo1234 ", "u136736371_kairoo");
 
 if (!$connection) {
     echo "Erro" . mysqli_connect_error();
